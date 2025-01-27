ef239d0f14c4e3c68ff826f8e4e7052c2024011a
Nicolas Paiuca Buscarini <nicolasbuscarini@hotmail.com>
2025-01-27 08:45:31 -0300

    FEAT: Add new packages and EnumDescriptionConverter class
    Updated CodeFlow.Start.Lib.csproj to include CsvHelper, FluentValidation, Microsoft.AspNetCore.Authentication.JwtBearer, and Microsoft.AspNetCore.Mvc.Versioning packages. Added EnumDescriptionConverter<T> class in CodeFlow.Start.Lib.Helper.Csv namespace for custom enum conversion using CsvHelper.

---
+++ CodeFlow.Start.Lib/Helper/Csv/EnumDescriptionConverter.cs
@@ -0,0 +1,45 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+using System;
+using System.ComponentModel;
+
+namespace CodeFlow.Start.Lib.Helper.Csv;
+public class EnumDescriptionConverterr<T> : DefaultTypeConverter where T : struct, Enum
+{
+    public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+    {
+        foreach (var field in typeof(T).GetFields())
+        {
+            if (Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) is DescriptionAttribute attribute)
+            {
+                if (attribute.Description == text)
+                {
+                    return (T)field.GetValue(null);
+                }
+            }
+            else
+            {
+                if (field.Name == text)
+                {
+                    return (T)field.GetValue(null);
+                }
+            }
+        }
+        throw new ArgumentException($"Unknown description '{text}' for enum '{typeof(T).Name}'");
+    }
+
+    public override string? ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
+    {
+        if (value is T enumValue)
+        {
+            var field = typeof(T).GetField(enumValue.ToString());
+            if (Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) is DescriptionAttribute attribute)
+            {
+                return attribute.Description;
+            }
+            return enumValue.ToString();
+        }
+        return base.ConvertToString(value, row, memberMapData);
+    }
+}
